329fd65dff79a75619791ef7d144db0de2d0c837
Daniel Burruchaga <dbs99@hotmail.com>
2022-01-04 16:58:08 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+from subprocess import PIPE
+command = "ping google.es"
+process = subprocess.Popen(
+command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+while True:
+    line = process.stdout.readline()
+    if not line:
+        break
+  #the real code does filtering here
+    print("test:", line.rstrip())
+  
+    if(line != ''):
+        sys.stdout.write(str(line))
+        sys.stdout.flush()
+        if(str(line).count("rx")):
+            print("RX_LORA")
+        if(str(line).count("tx")):
+            print("TX_LORA")
