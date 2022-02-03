11fea9ffcf99f2ec6e720f57a9ad49a1afbb1b35
Daniel Burruchaga <dbs99@hotmail.com>
2022-02-03 18:38:46 +0100

    Create test_lora.py

---
+++ test_lora.py
@@ -0,0 +1,37 @@
+#RPI.GPIO as GPIO
+import os
+import json
+import glob
+import psutil
+import subprocess
+import sys
+from subprocess import PIPE
+######################################################
+print("Creando Proceso Lora")
+command = "journalctl -u gesinen-sentilo-connector -f"
+process = subprocess.Popen(
+    command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+
+while(True):
+#####################################################
+#TX_ RX LORA
+# #######################################################
+    print("Creando Proceso Lora")
+    command = "journalctl -u gesinen-sentilo-connector -f"
+    line = process.stdout.readline()
+    if not line:
+        break
+  #the real code does filtering here
+    print("LORA:", line.rstrip())
+  
+    if(line != ''):
+       
+        if(str(line).count("### RECEIVED APPLICATION MESSAGE ###")):
+            #Modules.Serial_me.SerialWrite(portConfig,"3_RX") #RX
+            print("RX_LORA")
+            sys.stdout.write(str(line))
+            sys.stdout.flush()
+        if(str(line).count("tx")):
+            #Modules.Serial_me.SerialWrite(portConfig,"3_TX") #TX
+            print("TX_LORA")
+
