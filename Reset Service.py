78e0573a83f2d543e5e520240523357c36df8031
Daniel Burruchaga <dbs99@hotmail.com>
2022-02-04 12:52:00 +0100

    Update Reset Service.py

--- Reset Service.py
+++ Reset Service.py
@@ -79,12 +79,12 @@ while(True):
     #print("LORA:", line.rstrip()
     if(line != ''):
         if ("### RECEIVED APPLICATION MESSAGE ###" in str(line)) : #SI QUE VA 
-            #Modules.Serial_me.SerialWrite(portConfig,"3_RX") #RX
+            Modules.Serial_me.SerialWrite(portConfig,"3_RX") #RX
             print("RX_LORA")
             sys.stdout.write(str(line))
             sys.stdout.flush()
         if(str(line).count("tx")): ### NO SE ENVIA POR JOURNAL NO VA TX  PERO CADA VEZ QUE RECIBE SI QUE VA 
-            #Modules.Serial_me.SerialWrite(portConfig,"3_TX") #TX
+            Modules.Serial_me.SerialWrite(portConfig,"3_TX") #TX
             print("TX_LORA")
 #####################################################
 #CPU
